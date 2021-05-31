17b28287262893cc5d1e76f6c77e19659b0dd175
Chiogros <alexandre.bouijoux@gmail.com>
2021-05-31 20:32:20 +0200

    backup before converting List<String> as JSONObject for answers

--- app/src/main/java/com/example/loustics/models/Litteral.java
+++ app/src/main/java/com/example/loustics/models/Litteral.java
@@ -4,60 +4,21 @@ import android.content.Context;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.room.ColumnInfo;
-import androidx.room.ForeignKey;
-import androidx.room.Ignore;
-import androidx.room.Index;
-import androidx.room.PrimaryKey;
-import androidx.room.Entity;
-import java.util.ArrayList;
 import java.util.List;
 
-@Entity(tableName = "Litteral",
-foreignKeys = {
-    @ForeignKey(entity = Chapter.class, parentColumns = { "name", "courseName" }, childColumns = { "chapterName", "courseName" })
-},
-primaryKeys = {
-    "subject", "chapterName", "courseName"
-},
-indices = {
-    @Index("subject"),
-    @Index("chapterName"),
-    @Index("courseName")
-})
 public class Litteral extends Question {
 
-    @NonNull
-    @ColumnInfo (name = "subject")
-    private String m_s_subject;
-
-    @NonNull
-    @ColumnInfo (name = "chapterName")
-    private String m_s_chapterName;
-
-    @NonNull
-    @ColumnInfo (name = "courseName")
-    private String m_s_courseName;
-
-    @ColumnInfo (name = "answers")
-    private List<String> m_al_answers;
-
-
     public Litteral(String m_s_subject, String m_s_chapterName, String m_s_courseName, List<String> m_al_answers) {
-        this.m_s_subject = m_s_subject;
-        this.m_s_chapterName = m_s_chapterName;
-        this.m_s_courseName = m_s_courseName;
-        this.m_al_answers = m_al_answers;
+        super(m_s_subject, m_s_chapterName, m_s_courseName, m_al_answers);
     }
 
     public String getAlternativeAnswer() {
         int randomBadAnswer = (int) (1 + Math.random() * 3);
-        return m_al_answers.get(randomBadAnswer);
+        return m_l_answers.get(randomBadAnswer);
     }
 
     public String getAnswer() {
-        return m_al_answers.get(0);
+        return ;
     }
 
     @Override
@@ -67,25 +28,9 @@ public class Litteral extends Question {
         return tv;
     }
 
-    public List<String> getM_al_answers() {
-        return this.m_al_answers;
-    }
-
-    public String getM_s_chapterName() {
-        return this.m_s_chapterName;
-    }
-
-    public String getM_s_courseName() {
-        return this.m_s_courseName;
-    }
-
-    public String getM_s_subject() {
-        return this.m_s_subject;
-    }
-
     public View getSubjectView(Context context) {
         TextView tv = new TextView(context);
-        tv.setText(this.m_s_subject);
+        tv.setText(getM_s_subject());
         return tv;
     }
 }
--- app/src/main/java/com/example/loustics/models/Question.java
+++ app/src/main/java/com/example/loustics/models/Question.java
@@ -2,13 +2,55 @@ package com.example.loustics.models;
 
 import android.content.Context;
 import android.view.View;
-import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Ignore;
+import androidx.room.Index;
 
 import java.util.List;
 
+
+@Entity(tableName = "Question",
+foreignKeys = {
+    @ForeignKey(entity = Chapter.class, parentColumns = { "name", "courseName" }, childColumns = { "chapterName", "courseName" })
+},
+primaryKeys = {
+    "subject", "chapterName", "courseName"
+},
+indices = {
+    @Index("subject"),
+    @Index("chapterName"),
+    @Index("courseName")
+})
 public abstract class Question {
 
-    protected Question() {}
+    @NonNull
+    @ColumnInfo(name = "subject")
+    private String m_s_subject;
+
+    @NonNull
+    @ColumnInfo (name = "chapterName")
+    private String m_s_chapterName;
+
+    @NonNull
+    @ColumnInfo (name = "courseName")
+    private String m_s_courseName;
+
+    @ColumnInfo (name = "answers")
+    private List<String> m_l_answers;
+
+    @Ignore
+    private List<String> m_l_rightAnswers, m_l_wrongAnswers;
+
+    public Question(String m_s_subject, String m_s_chapterName, String m_s_courseName, List<String> m_l_answers) {
+        this.m_s_subject = m_s_subject;
+        this.m_s_chapterName = m_s_chapterName;
+        this.m_s_courseName = m_s_courseName;
+        this.m_l_answers = m_l_answers;
+    }
 
     public abstract Object getAlternativeAnswer();
 
@@ -16,12 +58,22 @@ public abstract class Question {
 
     public abstract View getAnswerView(Context context, Object answer);
 
-    public abstract Object getM_s_subject();
+    public List<String> getM_l_answers() {
+        return this.m_l_answers;
+    }
+
+    public String getM_s_chapterName() {
+        return this.m_s_chapterName;
+    }
+
+    public String getM_s_courseName() {
+        return this.m_s_courseName;
+    }
+
+    public String getM_s_subject() {
+        return this.m_s_subject;
+    }
 
     public abstract View getSubjectView(Context context);
-
-    public static List<Question> getAllQuestions(String chapterName, String courseName) {
-
-    }
 
 }
