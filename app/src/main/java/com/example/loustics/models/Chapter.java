4180741d61af880822e52b42c71f3b9074a20bd3
Chiogros <alexandre.bouijoux@gmail.com>
2021-06-11 11:32:10 +0200

    resolved all Chapter warnings (@NotNull and finals), excepted for lesson attribute and methods that would be useful for further updates

--- app/src/main/java/com/example/loustics/models/Chapter.java
+++ app/src/main/java/com/example/loustics/models/Chapter.java
@@ -7,6 +7,8 @@ import androidx.room.ForeignKey;
 import androidx.room.Ignore;
 import androidx.room.Index;
 
+import org.jetbrains.annotations.NotNull;
+
 @Entity(tableName = "Chapter",
 foreignKeys = {
     @ForeignKey(entity = Course.class, parentColumns = "name", childColumns = "courseName")
@@ -22,39 +24,33 @@ public class Chapter {
 
     @NonNull
     @ColumnInfo(name="name")
-    private String m_s_name;
+    private final String m_s_name;
 
     @NonNull
     @ColumnInfo(name = "courseName")
-    private String m_s_courseName;
+    private final String m_s_courseName;
 
     @Ignore
     private Lesson m_l_lesson;
 
 
-    public Chapter(String m_s_name, String m_s_courseName) {
+    public Chapter(@NotNull String m_s_name, @NotNull String m_s_courseName) {
         this.m_s_name = m_s_name;
         this.m_s_courseName = m_s_courseName;
     }
 
-    public Lesson getLesson() {
+    public Lesson getM_l_lesson() {
         return this.m_l_lesson;
     }
 
+    @NotNull
     public String getM_s_courseName() {
         return this.m_s_courseName;
     }
 
+    @NotNull
     public String getM_s_name() {
         return this.m_s_name;
-    }
-
-    public void setLesson(Lesson lesson) {
-        this.m_l_lesson = lesson;
-    }
-
-    public void setM_s_name(String name) {
-        this.m_s_name = name;
     }
 
 }
