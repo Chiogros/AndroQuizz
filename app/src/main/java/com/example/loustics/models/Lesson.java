c17406b49957cf8b946225e1ec0cbc33fd2d9ca0
Chiogros <alexandre.bouijoux@gmail.com>
2021-06-11 11:48:20 +0200

    Resolved all Litteral warnings (@Nonnull and finals)

--- app/src/main/java/com/example/loustics/models/Lesson.java
+++ app/src/main/java/com/example/loustics/models/Lesson.java
@@ -1,4 +1,5 @@
 package com.example.loustics.models;
 
 public class Lesson {
+    // à mettre en place dans de futures MAJs !
 }
--- app/src/main/java/com/example/loustics/models/Litteral.java
+++ app/src/main/java/com/example/loustics/models/Litteral.java
@@ -12,6 +12,7 @@ import androidx.room.ForeignKey;
 import androidx.room.Ignore;
 import androidx.room.Index;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -33,24 +34,25 @@ public class Litteral extends Question {
 
     @NonNull
     @ColumnInfo(name = "subject")
-    private String m_s_subject;
+    private final String m_s_subject;
 
     @NonNull
     @ColumnInfo (name = "chapterName")
-    private String m_s_chapterName;
+    private final String m_s_chapterName;
 
     @NonNull
     @ColumnInfo (name = "courseName")
-    private String m_s_courseName;
+    private final String m_s_courseName;
 
+    @NonNull
     @ColumnInfo (name = "answers")
-    private JSONObject m_json_answers;
+    private final JSONObject m_json_answers;
 
     @Ignore
     public static ArrayList<Class<? extends QuestionFrame>> QuestionsFramesCompatibleWith = new ArrayList<>();
 
 
-    public Litteral(String m_s_subject, String m_s_chapterName, String m_s_courseName, JSONObject m_json_answers) {
+    public Litteral(@NotNull String m_s_subject, @NotNull String m_s_chapterName, @NotNull String m_s_courseName, @NotNull JSONObject m_json_answers) {
         this.m_s_subject = m_s_subject;
         this.m_s_chapterName = m_s_chapterName;
         this.m_s_courseName = m_s_courseName;
@@ -60,7 +62,7 @@ public class Litteral extends Question {
             m_json_wrongAnswers = m_json_answers.getJSONArray("wrong");
         } catch (JSONException e) {
             e.printStackTrace();
-            Log.d("*****", "Impossible de parser correctement pour le sujet : " + m_s_subject);
+            Log.d("*****", "Impossible de parser le sujet : '" + m_s_subject + "'");
         }
     }
 
@@ -85,18 +87,22 @@ public class Litteral extends Question {
         return tv;
     }
 
+    @NotNull
     public JSONObject getM_json_answers() {
         return this.m_json_answers;
     }
 
+    @NotNull
     public String getM_s_chapterName() {
         return this.m_s_chapterName;
     }
 
+    @NotNull
     public String getM_s_courseName() {
         return this.m_s_courseName;
     }
 
+    @NotNull
     public String getM_s_subject() {
         return this.m_s_subject;
     }
