270703480e13f7f49104c62d3d908478895f456a
Chiogros <alexandre.bouijoux@gmail.com>
2021-06-11 11:37:05 +0200

    Resolved all Course warnings (added some @NotNull and finals)

--- app/src/main/java/com/example/loustics/models/Course.java
+++ app/src/main/java/com/example/loustics/models/Course.java
@@ -6,32 +6,37 @@ import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 import androidx.room.Entity;
 
+import org.jetbrains.annotations.NotNull;
+
 @Entity(tableName = "Course")
 public class Course {
 
-    @PrimaryKey @NonNull
+    @PrimaryKey @NotNull
     @ColumnInfo(name="name")
-    private String m_s_name;
+    private final String m_s_name;
 
+    @NotNull
     @ColumnInfo(name="logo")
-    private String m_s_logo;
+    private final String m_s_logo;
 
 
     @Ignore
-    public Course(String m_s_name) {
+    public Course(@NotNull String m_s_name) {
         this.m_s_logo = "";
         this.m_s_name = m_s_name;
     }
 
-    public Course(String m_s_name, String m_s_logo) {
+    public Course(@NotNull String m_s_name, @NotNull String m_s_logo) {
         this.m_s_logo = m_s_logo;
         this.m_s_name = m_s_name;
     }
 
+    @NotNull
     public String getM_s_logo() {
         return this.m_s_logo;
     }
 
+    @NotNull
     public String getM_s_name() {
         return this.m_s_name;
     }
